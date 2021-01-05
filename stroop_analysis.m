163724358e13221f5adae837493eede639babb08
dorian <dorian@mino.rs>
2021-01-05 14:05:48 +0000

    add functionality for jatos composite files; update filtering

--- stroop_analysis.m
+++ stroop_analysis.m
@@ -20,7 +20,7 @@ datadir = fullfile(rootdir,'data/pilot_1');
 p.savefilename = 'processed_data';
 p.datafilepattern = 'jatos_results_*';
 p.keycodes = [1,2,3;49,50,51]; % JS keycode mappings
-d.legend = {1,2,3,4;'red','blue','green',NaN;'short','medium','tall',NaN;'congruent','incongruent',NaN,NaN; 'size', 'colour','size_only','colour_only'}; % keep a record of the codes we'll work with
+d.legend = {1,2,3,4;'red','blue','green',NaN;'short','medium','tall',NaN;'congruent','incongruent',NaN,NaN; 'size', 'colour','size_only','colour_only';'falsefont','font',NaN,NaN}; % keep a record of the codes we'll work with
 
 % directory mapping
 addpath(genpath(fullfile(rootdir, 'tools'))); % add tools folder to path (don't think we need this, but in case)
@@ -29,23 +29,33 @@ save_file = fullfile(datadir, p.savefilename);
 
 %% loop through subjects
 d.fileinfo = dir(fullfile(datadir, p.datafilepattern)); % find all the datafiles and get their info
-for subject = 1:length(d.fileinfo) % loop through each subject
-    t.path = fullfile(datadir, d.fileinfo(subject).name); % get the full path to the file
+t.alldata = {};
+for file = 1:length(d.fileinfo)
+    t.path = fullfile(datadir, d.fileinfo(file).name); % get the full path to the file
     fprintf(1, 'working with %s\n', t.path); % print that so you can check
     
-    t.alldata = loadjson(t.path); % load in the data
+    t.load = loadjson(t.path); % load in the data
     
-    t.id = t.alldata{1}.unique_id; % since we generated unique ids we'll pull these in
+    t.alldata = [t.alldata,t.load]; % concat those into one var, so each subject is a cell
+end
+d.alldata = t.alldata; % save all the data
+    
+for subject = 1:length(t.alldata) % loop through each subject
+    fprintf(1, 'working with subject %f\n', subject); % print that so you can check
+
+    t.this_subj_data = t.alldata{subject};
+    
+    t.id = t.this_subj_data{1}.unique_id; % since we generated unique ids we'll pull these in
     
     % init a couple of counters
     t.testcounter = 0;
     t.traincounter = 0;
     %% loop through trials
     % data is all in a row, so we go through each col and pull the values we want
-    for trial = 1:length(t.alldata)
+    for trial = 1:length(t.this_subj_data)
         clear t.current_trial t.curr;
         
-        t.current_trial = t.alldata{trial};
+        t.current_trial = t.this_subj_data{trial};
         
         % don't worry about unlabelled trials, or fixation trials
         if isfield(t.current_trial, 'exp_part')
@@ -53,7 +63,7 @@ for subject = 1:length(d.fileinfo) % loop through each subject
                 
                 % get rt
                 if isempty(t.current_trial.rt)
-                    t.curr.rt = 0;
+                    t.curr.rt = NaN;
                 else
                     t.curr.rt = t.current_trial.rt;
                 end
@@ -61,8 +71,10 @@ for subject = 1:length(d.fileinfo) % loop through each subject
                 
                 %%  this will need to change for vocal
                 % get response (convert from JS keycode using p.keycodes
-                if isempty(t.current_trial.key_press) % if this isn't empty
-                    t.curr.resp = 0;
+                if isempty(t.current_trial.key_press) % if response is empty
+                    t.curr.resp = NaN;
+                elseif isempty(p.keycodes(1,find(p.keycodes(2,:) == t.current_trial.key_press))) % else if response is not valid
+                    t.curr.resp = t.current_trial.key_press; % save the javascript keycode
                 else
                     t.curr.resp = p.keycodes(1,find(p.keycodes(2,:) == t.current_trial.key_press));
                 end
@@ -71,6 +83,11 @@ for subject = 1:length(d.fileinfo) % loop through each subject
                 t.curr.stim_size = convertCharsToStrings(t.current_trial.stim_data.size);
                 t.curr.stim_colour = convertCharsToStrings(t.current_trial.stim_data.colour);
                 t.curr.stim_print = convertCharsToStrings(t.current_trial.stim_data.print);
+                if startsWith(t.curr.stim_print,'ff')
+                    t.curr.stim_type = 'falsefont';
+                else
+                    t.curr.stim_type = 'font';
+                end
                 t.curr.congruency = convertCharsToStrings(t.current_trial.stim_data.congruency);
                 
                 % get test information
@@ -120,6 +137,13 @@ for subject = 1:length(d.fileinfo) % loop through each subject
                 elseif strcmp(t.curr.test_type,d.legend{5,4})
                     t.curr.allcodes(:,6) = 4;
                 end
+
+                t.curr.all(:,7) = {t.curr.stim_type};
+                if strcmp(t.curr.stim_type,d.legend{6,1})
+                    t.curr.allcodes(:,7) = 1;
+                elseif strcmp(t.curr.stim_type,d.legend{6,2})
+                    t.curr.allcodes(:,7) = 2;
+                end
                 
                 %% load the sorted data according to experiment part
                 if strcmp(t.current_trial.exp_part, 'testing') % put test data in one var
@@ -141,20 +165,34 @@ for subject = 1:length(d.fileinfo) % loop through each subject
     
     %% collate the outputs per subject
     d.subjects(subject).id = t.id;
-    d.subjects(subject).JSONdata = t.alldata;
+    d.subjects(subject).JSONdata = t.this_subj_data;
     d.subjects(subject).testdata = t.testdata;
     d.subjects(subject).traindata = t.traindata;
+    d.subjects(subject).results = []; % init this
     
     %% do some filtering
-    t.size_trials = filter_data(d.subjects(subject).testdata.allcodes,'sizes');
-    t.congruent_size_trials = filter_data(t.size_trials,'congruent');
-    t.incongruent_size_trials = filter_data(t.size_trials,'incongruent');
-    [mean(t.congruent_size_trials(1,:)),mean(t.incongruent_size_trials(1,:))]
+    t.results = [];
     
-    t.colour_trials = filter_data(d.subjects(subject).testdata.allcodes,'colour');
-    t.congruent_colour_trials = filter_data(t.colour_trials,'congruent');
-    t.incongruent_colour_trials = filter_data(t.colour_trials,'incongruent');
-    [mean(t.congruent_colour_trials(1,:)),mean(t.incongruent_colour_trials(1,:))]
+    t.results.size = filter_data(d.subjects(subject).testdata.allcodes,'sizes');
+    t.results.size_congruent = filter_data(t.results.size,'congruent');
+    t.results.size_congruent_falsefont = filter_data(t.results.size_congruent,'falsefont');
+    t.results.size_congruent_font = filter_data(t.results.size_congruent,'font');
+    t.results.size_incongruent = filter_data(t.results.size,'incongruent');
+    t.results.size_incongruent_falsefont = filter_data(t.results.size_incongruent,'falsefont');
+    t.results.size_incongruent_font = filter_data(t.results.size_incongruent,'font');
+    
+    t.results.colour = filter_data(d.subjects(subject).testdata.allcodes,'colour');
+    t.results.colour_congruent = filter_data(t.results.colour,'congruent');
+    t.results.colour_congruent_falsefont = filter_data(t.results.colour_congruent,'falsefont');
+    t.results.colour_congruent_font = filter_data(t.results.colour_congruent,'font');
+    t.results.colour_incongruent = filter_data(t.results.colour,'incongruent');
+    t.results.colour_incongruent_falsefont = filter_data(t.results.colour_incongruent,'falsefont');
+    t.results.colour_incongruent_font = filter_data(t.results.colour_incongruent,'font');
+    
+    t.results.means = [mean(t.results.colour_congruent(1,:),'omitnan'),mean(t.results.colour_incongruent(1,:),'omitnan');mean(t.results.colour_congruent_falsefont(1,:),'omitnan'),mean(t.results.colour_incongruent_falsefont(1,:),'omitnan');mean(t.results.colour_congruent_font(1,:),'omitnan'),mean(t.results.colour_incongruent_font(1,:),'omitnan');mean(t.results.size_congruent(1,:),'omitnan'),mean(t.results.size_incongruent(1,:),'omitnan');mean(t.results.size_congruent_falsefont(1,:),'omitnan'),mean(t.results.size_incongruent_falsefont(1,:),'omitnan');mean(t.results.size_congruent_font(1,:),'omitnan'),mean(t.results.size_incongruent_font(1,:),'omitnan')];
+    disp(t.results.means);
+    
+    d.subjects(subject).results = t.results;
     
 end
 
@@ -172,6 +210,10 @@ function filtered_data = filter_data(data,filter)
             idx = find(data(5,:) == 1); % size info
         case 'colour'
             idx = find(data(5,:) == 2); % colour info
+        case 'falsefont'
+            idx = find(data(7,:) == 1); % print info
+        case 'font'
+            idx = find(data(7,:) == 2); % print info
     end
     
     filtered_data = data(:,idx);
